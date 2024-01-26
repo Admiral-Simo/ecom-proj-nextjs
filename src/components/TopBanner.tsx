ed327649cca0f5cfe9fbe1eeb98fd937a7f30471
Admiral-Simo <admiralsimo16@gmail.com>
2024-01-26 22:37:52 +0100

    style: finish the desktop version

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,9 +1,25 @@
+import { Instagram, Youtube, Twitter, Facebook } from "lucide-react";
+
 function TopBanner() {
   return (
-    <div className="flex items-center justify-between">
-      <p className="uppercase">new cartel series now</p>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">{/*socials right here*/}</div>
+    <div className="bg-gray-900">
+      <div className="flex items-center mx-auto max-w-screen-xl justify-between">
+        <p className="uppercase flex-1 text-center">
+          exclusive cartel collection <span>🇲🇽</span>
+        </p>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            {/*socials right here*/}
+            <Instagram />
+            <Youtube />
+            <Twitter />
+            <Facebook />
+          </div>
+          <div className="flex items-center gap-1 ml-10">
+            <p className="text-2xl">🇲🇦</p>
+            <p className="font-thin">Morocco (MAD)</p>
+          </div>
+        </div>
       </div>
     </div>
   );
