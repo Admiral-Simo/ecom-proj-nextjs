eaee5f2dc12326bf5b4f11894286d76cd2444bf3
Admiral-Simo <admiralsimo16@gmail.com>
2024-01-26 22:01:43 +0100

    style: switch to dark color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl text-yellow-500 font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">Hello World</h1>
     </main>
   );
 }
