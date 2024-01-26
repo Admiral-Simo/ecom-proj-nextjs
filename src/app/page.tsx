ba5e4b64738310635edb5bf2f958b2a9d29a8bcb
Admiral-Simo <admiralsimo16@gmail.com>
2024-01-26 22:20:17 +0100

    make TopBanner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import TopBanner from "@/components/TopBanner";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <main>
+      <TopBanner />
     </main>
   );
 }
---
+++ src/components/TopBanner.tsx
@@ -0,0 +1,12 @@
+function TopBanner() {
+  return (
+    <div className="flex items-center justify-between">
+      <p className="uppercase">new cartel series now</p>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">{/*socials right here*/}</div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBanner;
