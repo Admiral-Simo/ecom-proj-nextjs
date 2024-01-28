c2d785abf21302ab93e9faff8e1d61f05e5b24f6
Admiral-Simo <admiralsimo16@gmail.com>
2024-01-28 16:29:21 +0100

    style: change underline hover of link items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,18 @@ interface INavbar {
 
 function Navbar({ routes }: INavbar) {
   return (
-    <div className="flex items-center justify-between py-5 max-w-screen-xl mx-auto">
+    <div className="flex items-center justify-between px-3 py-5 max-w-screen-xl mx-auto">
       <div className="flex items-center gap-20">
         <h1 className="text-4xl font-bold">
-          simo<span className="text-red-500">shop</span>
+          simo<span className="text-blue-500">shop</span>
         </h1>
-        <div className="space-x-10">
+        <div className="space-x-10 hidden md:block">
           {routes.map((item) => (
-            <a className="tracking-widest" key={item.title} href={item.link}>
+            <a
+              className="relative tracking-widest hover:after:absolute  hover:after:top-6 hover:after:left-0 hover:after:w-full hover:after:h-0.5 hover:after:bg-blue-500 hover:after:content-'' hover:after:transition-all hover:after:duration-300"
+              key={item.title}
+              href={item.link}
+            >
               {item.title}
             </a>
           ))}
