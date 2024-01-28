fc7f0d7598cf833fa70b0f1b0271f72f10dd548a
Admiral-Simo <admiralsimo16@gmail.com>
2024-01-28 16:18:25 +0100

    use navbar in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Navbar from "@/components/Navbar";
 import TopBanner from "@/components/TopBanner";
+
+const routes = [
+  { title: "home", link: "/" },
+  { title: "connect & faq's", link: "/contact" },
+];
 
 export default function Home() {
   return (
     <main>
       <TopBanner />
+      <Navbar routes={routes} />
     </main>
   );
 }
