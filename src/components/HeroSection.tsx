6b3f4bf2d8ab4f9f72bf432654643fcbdc1be73f
Admiral-Simo <admiralsimo16@gmail.com>
2024-02-02 13:56:21 +0100

    make a hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,68 @@
+"use client";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+import { useRouter } from "next/navigation";
+
 interface IHero {
   shopRoute: string;
 }
 
+function MovingText({
+  text,
+  direction,
+}: {
+  text: string;
+  direction: "left" | "right";
+}) {
+  let count = 40;
+  return (
+    <Marquee direction={direction} pauseOnHover>
+      {
+        // Using a for loop to generate JSX elements
+        // Remember to provide a unique key to each element in the loop
+        // This helps React efficiently update the DOM
+        (() => {
+          const elements = [];
+          for (let i = 0; i < count; i++) {
+            elements.push(
+              <p
+                key={i}
+                className="uppercase font-thin tracking-widest text-lg mr-5"
+              >
+                {text}
+              </p>,
+            );
+          }
+          return elements;
+        })()
+      }
+    </Marquee>
+  );
+}
+
 function HeroSection({ shopRoute }: IHero) {
-  return <div>{shopRoute}</div>;
+  const router = useRouter();
+
+  const navigate = () => {
+    router.push(shopRoute, { scroll: false });
+  };
+
+  return (
+    <div className="w-full">
+      <MovingText text="new drop" direction="right" />
+      {/* here will be the hero image */}
+      <div className="relative">
+        <Image src="/dubai.jpeg" alt="Dubai" width={5000} height={300} />
+        <button
+          onClick={() => navigate()}
+          className="absolute bottom-16 text-lg font-bold right-16 py-3 px-5 bg-red-500 uppercase shadow-2xl shadow-red-500 rounded-full"
+        >
+          Shop Now
+        </button>
+      </div>
+      <MovingText text="🇲🇽 new cartel series 🇲🇽" direction="left" />
+    </div>
+  );
 }
 
 export default HeroSection;
